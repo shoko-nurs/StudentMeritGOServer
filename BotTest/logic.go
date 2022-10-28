587e6deb559313bab9c185144ff490b86b4f7739
shoko-nurs <shokonurs@gmail.com>
2022-10-28 14:08:56 +0600

    fix

--- BotTest/logic.go
+++ BotTest/logic.go
@@ -1,12 +1,11 @@
 package BotTest
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
-	"net/url"
 	"os"
 	"strconv"
-	"strings"
 )
 
 var Actions = map[string]interface{}{
@@ -19,9 +18,9 @@ var Actions = map[string]interface{}{
 
 func TelegramBotTest(w http.ResponseWriter, r *http.Request){
 
-
 	var update Update
 	json.NewDecoder(r.Body).Decode(&update)
+
 	text := update.Message.Text
 	userName := update.Message.From.Username
 	chatId := update.Message.Chat.Id
@@ -36,12 +35,15 @@ func TelegramBotTest(w http.ResponseWriter, r *http.Request){
 func SendTextToTelegram(chat_id int64, text string) (string){
 	telegramAPI := "https://api.telegram.org/bot"+os.Getenv("bot_token")+"/"+"sendMessage"
 
-	sendBody := url.Values{
-		"chat_id": {strconv.FormatInt(chat_id,10)},
-		"text": {text},
-
+	bodyOBj := map[string]string{
+		"chat_id": strconv.FormatInt(chat_id,10),
+		"text":text,
 	}
-	req, _ := http.NewRequest("POST", telegramAPI, strings.NewReader(sendBody.Encode()))
+
+	marsh,_ := json.Marshal(bodyOBj)
+	bodyReader := bytes.NewReader(marsh)
+
+	req, _ := http.NewRequest("POST", telegramAPI, bodyReader)
 
 	hc := http.Client{}
 	req.Header.Add("Content-Type", "application/json")
